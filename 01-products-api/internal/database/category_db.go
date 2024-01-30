1155130b7c865a66b51758a6abf42a36642feef5
Bruno Duarte <brunosduarte@hotmail.com>
2024-01-29 23:31:20 -0300

    creating products and routes

--- 01-products-api/internal/database/category_db.go
+++ 01-products-api/internal/database/category_db.go
@@ -31,3 +31,20 @@ func (cd *CategoryDB) GetCategories() ([]*entity.Category, error) {
 	}
 	return categories, nil
 }
+
+func (cd *CategoryDB) GetCategory(id string) (*entity.Category, error) {
+	var category entity.Category
+	err := cd.db.QueryRow("SELECT id, name FROM categories WHERE id = ?", id).Scan(&category.ID, &category.Name)
+	if err != nil {
+		return nil, err
+	}
+	return &category, nil
+}
+
+func (cd *CategoryDB) CreateCategory(category *entity.Category) (string, error) {
+	_, err := cd.db.Exec("INSERT INTO categories (id, name) VALUES (?, ?)", category.ID, category.Name)
+	if err != nil {
+		return "", err
+	}
+	return category.ID, nil
+}
---
+++ 01-products-api/internal/database/product_db.go
@@ -0,0 +1,72 @@
+package database
+
+import (
+	"database/sql"
+
+	"github.com/brunoduarte/full-cycle-week/internal/entity"
+)
+
+type ProductDB struct {
+	db *sql.DB
+}
+
+func NewProductDB(db *sql.DB) *ProductDB {
+	return &ProductDB{db: db}
+}
+
+func (pd *ProductDB) GetProducts() ([]*entity.Product, error) {
+	rows,err := pd.db.Query("SELECT id, name, price, category_id FROM products")
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	var products []*entity.Product
+	for rows.Next() {
+		var product entity.Product
+		if err := rows.Scan(&product.ID, &product.Name, &product.Price, &product.CategoryID); err != nil {
+			return nil, err
+		}
+		products = append(products, &product)
+	}
+	return products, nil
+}
+
+func (pd *ProductDB) GetProduct(id string) (*entity.Product, error) {
+	var product entity.Product
+	err := pd.db.QueryRow("SELECT id, name, price, category_id, image_url FROM products WHERE id = ?", id).
+		Scan(&product.ID, &product.Name, &product.Price, &product.CategoryID, &product.ImageURL)
+	if err != nil {
+		return nil, err
+	}
+
+	return &product, nil
+}
+
+func (pd *ProductDB) GetProductByCategoryID(categoryID string) ([]*entity.Product, error) {
+	rows, err := pd.db.Query("SELECT id, name, description, price, category_id, image_url FROM products WHERE category_id = ?", categoryID)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	var products []*entity.Product
+	for rows.Next() {
+		var product entity.Product
+		if err := rows.Scan(&product.ID, &product.Name, &product.Description, &product.Price, &product.CategoryID, &product.ImageURL); err != nil {
+			return nil, err
+		}
+		products = append(products, &product)
+	}
+	return products, nil
+}
+
+
+
+func (pd *ProductDB) CreateProduct(product *entity.Product) (string, error) {
+	_, err := pd.db.Exec("INSERT INTO products (id, name) VALUES (?, ?)", product.ID, product.Name)
+	if err != nil {
+		return "", err
+	}
+	return product.ID, nil
+}
